ccbf6f5075bdbce6395132637a63de2a9010b594
米诺 <1213235865@qq.com>
2025-11-21 19:40:53 +0800

    Update api-proxy.php

--- api-proxy.php
+++ api-proxy.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @author    校长bloG <1213235865@qq.com>
+ * @github    https://github.com/vpsaz/Mvideo-v2
+ */
+
 $config_file = __DIR__ . '/config/config.php';
 $conf = include($config_file);
 
